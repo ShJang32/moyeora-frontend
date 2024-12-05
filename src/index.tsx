2ff0cc5067d811cdf6bdefceb55838f5a99fc67f
ShJang32 <jangseohie32@gamil.com>
2024-12-05 14:13:59 +0900

    projectsetting1

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Router from './Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <Router/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
